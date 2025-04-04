8c7c85be6e374a8460b954bdede6f45f4506f38f
Tanvir Rahman <mdtanvirt@gmail.com>
2025-04-04 17:35:12 +0600

    Project initiate

---
+++ api/models.py
@@ -0,0 +1,43 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    class StatusChoice(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confiremed'
+        CANCELLED = 'Cancelled'
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product, through='OrderItem', related_name='order')
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quentity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quentity
+    
+    def __str__(self):
+        return f"{self.quentity} X {self.product.name} Order No {self.order.order_id}"
