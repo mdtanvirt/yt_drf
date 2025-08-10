320a6f7036a9f48feebde9d6acc4f21063b3c5f4
Tanvir Rahman <mdtanvirt@gmail.com>
2025-08-11 01:04:45 +0600

    NestedSerilizer, SerializerMethodFoield

--- api/serializers.py
+++ api/serializers.py
@@ -17,3 +17,30 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Price must be greater than 0."
             )
         return value
+
+class OrderItemSerizer(serializers.ModelSerializer):
+    #product = ProductSerializer()
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2)
+    class Meta:
+        model = OrderItem
+        fields = ('product_name', 'product_price', 'quentity', 'item_subtotal')
+
+class OrderSerilizer(serializers.ModelSerializer):
+    items = OrderItemSerizer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'created_at',
+            'user',
+            'status',
+            'items',
+            'total_price',
+        )
