1e7227454cc69e6965baaffb532f8fafb9365125
Tanvir Rahman <mdtanvirt@gmail.com>
2025-11-20 22:52:55 +0600

    Search, filter, ordering

--- api/views.py
+++ api/views.py
@@ -10,11 +10,29 @@ from rest_framework import status
 
 from rest_framework import generics
 
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .filter import ProductFileter
 
 class product_list(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    # Enable filtering, searching and ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = ProductFileter
+
+    # Filtering by specific fileds 
+    filterset_fileds = ['price', 'stock']
+
+    # searchng text-based filed
+    search_fields = ['name', 'description']
+
+    # Ordering filter
+    ordering_filter = ['price', 'stock', 'name']
+
+    # Default ordering
+    ordering = ['price']
 
 class order_list(viewsets.ModelViewSet):
     queryset = Order.objects.all().prefetch_related('items__product')
--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,17 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'api',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
